f0e8f301bded0840a3614dee4fd7ee32461e8eec
unknown <putraramadhan112004@gmail.com>
2023-11-28 15:11:02 +0700

    add contruct

--- PUTRARAMADAN_2255201005.php
+++ PUTRARAMADAN_2255201005.php
@@ -4,53 +4,51 @@
 
 <?php
 class Mahasiswa {
-  // Properties 
+
   private $Nama;
   private $NIM;
   private $MataKuliah;
   private $dosenkiller;
+  private $citacita;
 
-  // Methods
-  function set_Nama($Nomo) {
-    $this->Nama = $Nomo;
+
+  public function __construct($Nama, $NIM, $MataKuliah, $dosenkiller, $citacita){
+    $this->Nama = $Nama;
+    $this->NIM = $NIM;
+    $this->MataKuliah = $MataKuliah;
+    $this->dosenkiller = $dosenkiller;
+    $this->citacita = $citacita;
   }
+
+
   function get_Nama() {
     return $this->Nama;
-  }
-  function set_NIM($NIM){
-    $this-> NIM = $NIM;
   }
   function get_NIM(){
     return $this->NIM;
   }
-  function set_MataKuliah($MataKuliah){
-    $this-> MataKuliah = $MataKuliah;
-  }
   function get_MataKuliah(){
-    return $this -> MataKuliah;
-  }
-  function set_dosenkiller($dosenkiller){
-    $this -> dosenkiller = $dosenkiller;
+    return $this->MataKuliah;
   }
   function get_dosenkiller(){
-    return $this -> dosenkiller;
+    return $this->dosenkiller;
+  }
+    function get_citacita(){
+    return $this->citacita;
   }
 }
 
-$PutraRamadan = new Mahasiswa();
-$PutraRamadan->set_Nama('PutraRamadan');
+$PutraRamadan = new Mahasiswa('PutraRamadan', '2255201005', 'PBO', 'Citra', 'Youtuber');
 echo $PutraRamadan->get_Nama();
 echo "<br>";
-$PutraRamadan->set_NIM('2255201005');
 echo $PutraRamadan->get_NIM();
 echo "<br>";
-$PutraRamadan->set_MataKuliah('PBO');
 echo $PutraRamadan->get_MataKuliah();
 echo "<br>";
-$PutraRamadan->set_dosenkiller('Citra');
 echo $PutraRamadan->get_dosenkiller();
-
+echo "<br>";
+echo $PutraRamadan->get_citacita();
 ?>
- 
+
 </body>
 </html>
