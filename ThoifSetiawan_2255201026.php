8d84670ec67c62dd60d959d4f9ee1fdf4fd245ae
MuhammadThoifSetiawan <150098736+MuhammadThoifSetiawan@users.noreply.github.com>
2023-11-28 14:39:08 +0700

    ThoifSetiawan_2255201026.php

--- ThoifSetiawan_2255201026.php
+++ ThoifSetiawan_2255201026.php
@@ -1,4 +1,4 @@
-//Tugas Function
+
 
 <!DOCTYPE html>
 <html>
@@ -11,39 +11,36 @@ class Mahasiswa {
   private $NIM;
   private $Mata_Kuliah;
   private $Dosen_Killer;
+  private $cita_cita;
 
   // Methods
-  function set_Nama($Nama) {
+  function __construct ($Nama, $NIM, $Mata_Kuliah, $Dosen_Killer, $cita_cita ) {
     $this->Nama = $Nama;
+    $this->NIM = $NIM;
+    $this->Mata_Kuliah = $Mata_Kuliah;
+    $this->Dosen_Killer = $Dosen_Killer;
+    $this->cita_cita = $cita_cita;
   }
   function get_Nama() {
     return $this->Nama;
-  }
-  function set_NIM($NIM) {
-    $this->NIM = $NIM;
-  }
+ }
   function get_NIM() {
     return $this->NIM;
-  }
-  function set_Mata_Kuliah($Mata_Kuliah) {
-    $this->Mata_Kuliah = $Mata_Kuliah;
   }
   function get_Mata_Kuliah() {
     return $this->Mata_Kuliah;
   }
-  function set_Dosen_Killer($dk) {
-    $this->Dosen_Killer = $dk;
-  }
   function get_Dosen_Killer() {
     return $this->Dosen_Killer;
   }
+ function get_cita_cita() {
+    return $this->cita_cita;
+ }
 }
 
-$thoif = new Mahasiswa();
-$thoif->set_Nama('Thoif');
-$thoif->set_NIM('2255201026');
-$thoif->set_Mata_Kuliah('PBO');
-$thoif->set_Dosen_Killer('Pak_Arif');
+$thoif = new Mahasiswa("Thoif", "2255201026", "PBO", "Pak_Arif", "Belum
+_ada");
+
 
 echo $thoif->get_Nama();
 echo "<br>";
@@ -52,6 +49,8 @@ echo "<br>";
 echo $thoif->get_Mata_Kuliah();
 echo "<br>";
 echo $thoif->get_Dosen_Killer();
+echo "<br>";
+echo $thoif->get_cita_cita();
 ?>
  
 </body>
