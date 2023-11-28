ace4575118fd39cf804c757cdd725a7113cb74db
nabilaaarahmi <151619326+nabilaaarahmi@users.noreply.github.com>
2023-11-28 15:18:51 +0700

    Update nabila2155201020.php

--- nabila2155201020.php
+++ nabila2155201020.php
@@ -5,43 +5,37 @@ class Mahasiswa {
   public $NIM;
   public $Mata_Kuliah;
   public $Dosen_Killer;
-
+  public $Cita_cita;
 
   // Methods
-  function set_nama($nama) {
-    $this->nama = $nama;
+  function __construct($Nama, $Mata_kuliah, $Dosen_killer, $cita_cita){
+  $this->Nama = $Nama;
+  $this->NIM = $NIM;
+  $this->mata_kuliah  = $mata_kuliah;
+  $this->Dosen_killer = $Dosen_killer;
+  $this->cita_cita = $cita_cita;
   }
   function get_nama() {
     return $this->nama;
   }
-  function set_nim($NIM) {
-    $this->nim = $NIM;
-  }
   function get_nim() {
     return $this->nim;
-  }
-  function set_matkul($Mata_Kuliah) {
-    $this->matkul = $Mata_Kuliah;
   }
   function get_matkul() {
     return $this->matkul;
   }
-  function set_doskil($Dosen_Killer) {
-    $this->doskil = $Dosen_Killer;
-  }
   function get_doskil() {
     return $this->doskil;
   }
+  function get_cita() {
+    return $this->Cita_cita;
+  }
 }
 
-$nabila = new Mahasiswa();
-$nabila->set_nama('nabila rahmi');
+$nabila = new Mahasiswa('nabila rahmi', '2155201020', 'pemrograman berorientasi objek','istritae);
 echo $nabila->get_nama();
-$nabila->set_nim('2155202020');
-echo "\n";
 echo $nabila->get_nim();
-$nabila->set_matkul('PBO');
 echo $nabila->get_matkul();
-$nabila->set_doskil('pak arif mudi priyatno');
 echo $nabila->get_doskil();
+echo $nabila->get_cita();
 ?>
