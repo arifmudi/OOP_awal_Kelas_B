910ac7ef5e00f224dddc9b9d1115fefffeea4aa6
Shah Wiruddin <148735714+Shahwiruddin@users.noreply.github.com>
2023-11-28 15:22:22 +0700

    ShahWiruddin_2255201031.php

--- ShahWiruddin_2255201031.php
+++ ShahWiruddin_2255201031.php
@@ -4,53 +4,51 @@
 
 <?php
 class Mahasiswa {
-  // Properties 
+
   private $Nama;
   private $NIM;
   private $MataKuliah;
   private $dosenkiller;
+  private $citacita;
 
-  // Methods
-  function set_Nama($Nomo) {
-    $this->Nama = $Nomo;
+
+  public function __construct($Nama, $NIM, $MataKuliah, $dosenkiller, $citacita){
+    $this->Nama = $Nama;
+    $this->NIM = $NIM;
+    $this->MataKuliah = $MataKuliah;
+    $this->dosenkiller = $dosenkiller;
+    $this->citacita = $citacita;
   }
+
+
   function get_Nama() {
     return $this->Nama;
-  }
-  function set_NIM($NIM){
-    $this-> NIM = $NIM;
   }
   function get_NIM(){
     return $this->NIM;
   }
-  function set_MataKuliah($MataKuliah){
-    $this-> MataKuliah = $MataKuliah;
-  }
   function get_MataKuliah(){
-    return $this -> MataKuliah;
-  }
-  function set_dosenkiller($dosenkiller){
-    $this -> dosenkiller = $dosenkiller;
+    return $this->MataKuliah;
   }
   function get_dosenkiller(){
-    return $this -> dosenkiller;
+    return $this->dosenkiller;
+  }
+    function get_citacita(){
+    return $this->citacita;
   }
 }
 
-$ShahWiruddin = new Mahasiswa();
-$ShahWiruddin->set_Nama('ShahWiruddin');
-echo $ShahWiruddin->get_Nama();
+$Shahwiruddin = new Mahasiswa('Shahwiruddin', '2255201031', 'PBO', 'Pak Arif', 'JadiBola');
+echo $Shahwiruddin->get_Nama();
 echo "<br>";
-$ShahWiruddin->set_NIM('2255201031');
-echo $ShahWiruddin->get_NIM();
+echo $Shahwiruddin->get_NIM();
 echo "<br>";
-$ShahWiruddin->set_MataKuliah('PBO');
-echo $ShahWiruddin->get_MataKuliah();
+echo $Shahwiruddin->get_MataKuliah();
 echo "<br>";
-$ShahWiruddin->set_dosenkiller('Megawati');
-echo $ShahWiruddin->get_dosenkiller();
-
+echo $Shahwiruddin->get_dosenkiller();
+echo "<br>";
+echo $Shahwiruddin->get_citacita();
 ?>
- 
+
 </body>
 </html>
