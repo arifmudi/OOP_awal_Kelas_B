175067315ecc46e088ffdc8dedb30378eeb54419
djoellllllllllll <151618887+djoellllllllllll@users.noreply.github.com>
2023-11-28 14:20:25 +0700

    Update djoeljalalianifallah_2155201008.php

--- djoeljalalianifallah 2155201008.php
+++ djoeljalalianifallah 2155201008.php
@@ -5,41 +5,41 @@ class Mahasiswa {
   private $NIM;
   private $Mata_Kuliah;
   private $Dosen_Killer;
+  private $Cita_cita;
 
   // Methods
-  function set_nama($Name) {
-    $this->Nama = $Name;
+  function __construct($Nama, $NIM, $Mata_Kuliah, $Dosen_Killer, $Cita_cita){
+  $this->Nama = $Nama;
+  $this->NIM = $NIM; 
+  $this->Mata_Kuliah = $Mata_Kuliah;
+  $this->Dosen_Killer = $Dosen_Killer;
+  $this->Cita_cita = $Cita_cita;
   }
+
   function get_nama() {
     return $this->Nama;
-  }
-  function set_nim($NIM) {
-    $this->NIM = $NIM;
   }
   function get_nim() {
     return $this->NIM;
   }
-  function set_matkul($Mata_Kuliah) {
-    $this->Mata_Kuliah = $Mata_Kuliah;
-  }
   function get_matkul() {
     return $this->Mata_Kuliah;
-  }
-  function set_dokil($Dosen_Killer) {
-    $this->Dosen_Killer = $Dosen_Killer;
   }
   function get_dokil() {
     return $this->Dosen_Killer;
   }
+  function get_cita() {
+    return $this->Cita_cita;
+  }
+
+
 }
 
-$djoel = new Mahasiswa();
-$djoel->set_nama('Djoel jalalianifallah');
+
+$djoel = new Mahasiswa('Djoel jalalianifallah', '2155201008','program berorientasi objek','bapak arif mudi priyatno','jadi milyarder'  );
 echo $djoel->get_nama();
-$djoel->set_nim('2155201008');
 echo $djoel->get_nim();
-$djoel->set_matkul('program berorientasi objek');
 echo $djoel->get_matkul();
-$djoel->set_dokil('bapak arif mudi priyatno');
 echo $djoel->get_dokil();
+echo $djoel->get_cita();
 ?>
