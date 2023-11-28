97dee7dc9c5495929fae87068e2e7dc3f3949192
SRIUTAMI321 <150107382+SRIUTAMI321@users.noreply.github.com>
2023-11-28 15:04:44 +0700

    SRI_UTAMI(2255201032).php

---
+++ SRI_UTAMI(2255201032).php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+class Mahasiswa {
+
+  private $Nama;
+  private $NIM;
+  private $MataKuliah;
+  private $dosenkiller;
+  private $citacita;
+
+
+  public function __construct($Nama, $NIM, $MataKuliah, $dosenkiller, $citacita){
+    $this->Nama = $Nama;
+    $this->NIM = $NIM;
+    $this->MataKuliah = $MataKuliah;
+    $this->dosenkiller = $dosenkiller;
+    $this->citacita = $citacita;
+  }
+
+
+  function get_Nama() {
+    return $this->Nama;
+  }
+  function get_NIM(){
+    return $this->NIM;
+  }
+  function get_MataKuliah(){
+    return $this->MataKuliah;
+  }
+  function get_dosenkiller(){
+    return $this->dosenkiller;
+  }
+    function get_citacita(){
+    return $this->citacita;
+  }
+}
+
+$sriutami = new Mahasiswa('sriutami', '2255201032', 'PBO', 'amik', 'menjadi ibu bayangkari');
+echo $sriutami->get_Nama();
+echo "<br>";
+echo $sriutami->get_NIM();
+echo "<br>";
+echo $sriutami->get_MataKuliah();
+echo "<br>";
+echo $sriutami->get_dosenkiller();
+echo "<br>";
+echo $sriutami->get_citacita();
+?>
+
+</body>
+</html>
