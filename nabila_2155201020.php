11d443db4d9ee46e8825e8e68bfee658101c51cb
unknown <almanx11q@gmail.com>
2023-12-02 15:57:30 +0700

    nabila rahmi 2155201020 tugas5

--- nabila_2155201020.php
+++ nabila_2155201020.php
@@ -1,44 +1,62 @@
+<!DOCTYPE html>
+<html>
+<body>
+
 <?php
 class Mahasiswa {
   // Properties
   private $Nama;
   private $NIM;
   private $Mata_Kuliah;
-  private $cita_cita;
+  private $Dosen_Killer;
 
   // Methods
-  function __construct($Nama, $Nim, $Mata_kuliah, $Dosen_killer, $Cita_cita){
-    $this->Nama = $nama;
-    $this->Nim = $Nim;
-    $this->Mata_kuliah = $Mata_kuliah;
-    $this->Dosen_killer = $Dosen_killer;
-    $this->Cita_cita = $Cita_cita; 
+  function set_nama($Name) {
+    $this->Nama = $Name;
   }
-
   function get_nama() {
     return $this->Nama;
   }
   function set_nim($NIM) {
-    return $this->nim = $NIM;
+    $this->NIM = $NIM;
+  }
+  function get_nim() {
+    return $this->NIM;
+  }
+  function set_matkul($Mata_Kuliah) {
+    $this->Mata_Kuliah = $Mata_Kuliah;
   }
   function get_matkul() {
-    return $this->mata_kuliah;
+    return $this->Mata_Kuliah;
   }
-  function get_doskil() {
-   return $this->matkul = $Mata_Kuliah;
+  function set_dosen_killer($Dosen_Killer) {
+    $this->Dosen_Killer = $Dosen_Killer;
   }
-  function get_cita() {
-   return $this->ciita_cita;
+  function get_dosen_killer() {
+    return $this->Dosen_Killer;
   }
-  
-  
 }
 
-$nabila = new Mahasiswa('nabila rahmi', '2155201020','program berorientai objek','iSTRIjhe');
-echo $nabila->get_nama();
+$nabila = new Mahasiswa();
+$nabila->set_nama('nabila rahmi');
+echo $almann->get_nama();
+echo "\n";
+$nabila->set_nim('2255201020');
 echo $nabila->get_nim();
-$nabila->set_matkul();
-echo $nabila->get_matkul();
-$nabila->set_doskil();
-echo $nabila->get_doskil();
+echo "\n";
+$nabila->set_matkul('pbo');
+echo $almann->get_matkul();
+echo "\n";
+$nabila->set_dosen_killer('pak arif ');
+echo $almann->get_dosen_killer();
+echo "\n";
+
+
+
+
+
+
 ?>
+
+</body>
+</html>
