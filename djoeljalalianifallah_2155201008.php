6b8cd3a14938dbdc9a8c5f7946a2a27739843362
djoellllllllllll <151618887+djoellllllllllll@users.noreply.github.com>
2023-12-05 14:33:55 +0700

    djoeljalalianifallah_2155201008.php

--- djoeljalalianifallah_2155201008.php
+++ djoeljalalianifallah_2155201008.php
@@ -1,45 +1,41 @@
 <?php
-class Mahasiswa {
+class Universitas {
   // Properties
   private $Nama;
   private $NIM;
   private $Mata_Kuliah;
   private $Dosen_Killer;
   private $Cita_cita;
+  private $Hobi;
 
   // Methods
-  function __construct($Nama, $NIM, $Mata_Kuliah, $Dosen_Killer, $Cita_cita){
+  function __construct($Nama, $NIM, $Mata_Kuliah, $Dosen_Killer, $Cita_cita, $Hobi){
   $this->Nama = $Nama;
   $this->NIM = $NIM; 
   $this->Mata_Kuliah = $Mata_Kuliah;
   $this->Dosen_Killer = $Dosen_Killer;
   $this->Cita_cita = $Cita_cita;
+  $this->Hobi = $Hobi;
   }
 
-  function get_nama() {
-    return $this->Nama;
+  public function intro() {
+    echo "The Name is {$this->Nama} and the NIM is {$this->NIM} and the Mata Kuliah is {$this->Mata_Kuliah} and the Dosen Killer is {$this->Dosen_Killer} and the Cita cita is {$this->Cita_cita} and the Hobi is {$this->Hobi}."; 
   }
-  function get_nim() {
-    return $this->NIM;
-  }
-  function get_matkul() {
-    return $this->Mata_Kuliah;
-  }
-  function get_dokil() {
-    return $this->Dosen_Killer;
-  }
-  function get_cita() {
-    return $this->Cita_cita;
-  }
-
-
 }
+ 
+  // Mahasiswa is inherited from Universitas
+    class Mahasiswa extends Universitas {
+    public function message() {
+      echo "selamat datang dikampus kami "; 
+    }
+ }
 
 
-$djoel = new Mahasiswa('Djoel jalalianifallah', '2155201008','program berorientasi objek','bapak arif mudi priyatno','jadi milyarder'  );
-echo $djoel->get_nama();
-echo $djoel->get_nim();
-echo $djoel->get_matkul();
-echo $djoel->get_dokil();
-echo $djoel->get_cita();
+
+$Mahasiswa = new Mahasiswa('Djoel jalalianifallah', '2155201008','program berorientasi objek','bapak arif mudi priyatno','jadi milyarder','membaca webtoon');
+$Mahasiswa->message();
+echo "<br>";
+$Mahasiswa->intro();
 ?>
+
+
