eef404e0ef57f36b64ffc003b9e3b00c90ba5368
DINAMEYSICHANDRA <150003801+DINAMEYSICHANDRA@users.noreply.github.com>
2023-12-07 10:15:50 +0700

    Update dina_2255201057.php

--- dina_2255201057.php
+++ dina_2255201057.php
@@ -1,56 +1,64 @@
-<!DOCTYPE html>
-<html>
-<body>
-
 <?php
-class mahasiswa{
+class mahasiswa {
   // Properties
   private $name;
   private $nim;
-  private $mata_kuliah;
-  private $dosen_killer;
+  private $matakuliah;
+  private $dosenkiller;
   private $citacita;
+  private $hobby;
 
-  public function __construct($nama,$nim,$matakuliah,$dosenkiller,$citacita){
-  $this->nama = $nama;
-  $this->nim = $nim;
-  $this->matakuliah = $matakuliah;
-  $this->dosenkiller = $dosenkiller;
-  $this->citacita = $citacita;
+  // Constructor
+  public function __construct($name, $nim, $mata_kuliah, $dosen_killer, $citacita, $hobby) {
+    $this->nama = $name;
+    $this->nim = $nim;
+    $this->matakuliah = $mata_kuliah;
+    $this->dosenkiller = $dosen_killer;
+    $this->citacita = $citacita;
+    $this->hobby = $hobby;
+  }
+
   // Methods
   function get_name() {
-    return $this->name;
+    return $this->nama;
   }
   function get_nim() {
     return $this->nim;
   }
   function get_mata_kuliah() {
-    return $this->mata_kuliah;
+    return $this->matakuliah;
   }
   function get_dosen_killer() {
-    return $this->dosen_killer;
+    return $this->dosenkiller;
   }
   function get_citacita() {
     return $this->citacita;
   }
+  function get_hobby() {
+    return $this->hobby;
+  }
 }
 
-$dina = new mahasiswa('dina', '2255201057', 'pbo', 'PakArif');
+class universitas extends mahasiswa {
+  public function Salam() {
+    echo 'Selamat Datang Di Kampus Kami';
+  }
+}
 
+$dina = new universitas('dina', '2255201057', 'pbo', 'PakArif', 'jaya', 'makan');
+echo $dina->Salam();
+echo "<br>";
 echo $dina->get_name();
 echo "<br>";
-echo $dina ->get_nim();
+echo $dina->get_nim();
 echo "<br>";
 echo $dina->get_mata_kuliah();
 echo "<br>";
-echo $dina ->get_dosen_killer();
+echo $dina->get_dosen_killer();
 echo "<br>";
-echo $dina ->get_citacita();
-
-
-
-
+echo $dina->get_citacita();
+echo "<br>";
+echo $dina->get_hobby();
 ?>
- 
 </body>
 </html>
