df49f7d0d39e8ac657a69c770864800340b13ee4
SRIUTAMI321 <150107382+SRIUTAMI321@users.noreply.github.com>
2023-12-07 10:37:16 +0700

    Update SRI_UTAMI(2255201032).php

--- SRI UTAMI(2255201032).php
+++ SRI UTAMI(2255201032).php
@@ -10,13 +10,15 @@ class mahasiswa{
   private $matakuliah;
   private $dosenkiller;
   private $citacita;
+  private $Hobby;
 
-  public function __construct($name,$nim,$matakuliah,$dosenkiller,$citacita){
+  public function __construct($name,$nim,$matakuliah,$dosenkiller,$citacita, $Hobby){
     $this->name = $name;
     $this->nim = $nim;
     $this->matakuliah = $matakuliah;
     $this->dosenkiller = $dosenkiller;
     $this->citacita = $citacita;
+    $this->Hobby = $Hobby;
     }
     
   function get_name() {
@@ -31,12 +33,22 @@ class mahasiswa{
   function get_dosenkiller() {
     return $this->dosenkiller;
   }
- function get_citacita() {
+  function get_citacita() {
     return $this->citacita;
 	}
+  function get_Hobby() {
+    return $this->Hobby;
+	}
+}
+class Universitas extends Mahasiswa {
+  public function Salam() {
+    echo 'Selamat Datang Di Kampus Kami'; 
+  }
 }
 
-$sriutami = new mahasiswa('sriutami','2255201032','pbo','ArifMudiPriyanto.s.t.m.kom','ibupersit');
+$sriutami = new Universitas('sriutami','2255201032','pbo','ArifMudiPriyanto.s.t.m.kom','ibupersit','Healing');
+echo $sriutami->Salam();
+echo "<br>";
 echo $sriutami->get_name();
 echo "<br>";
 echo $sriutami->get_nim();
@@ -46,6 +58,8 @@ echo "<br>";
 echo $sriutami->get_dosenkiller();
 echo "<br>";
 echo $sriutami->get_citacita();
+echo "<br>";
+echo $sriutami->get_Hobby();
 
 ?>
  
