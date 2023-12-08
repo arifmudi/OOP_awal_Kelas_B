7929e1000eac2e1e87cc83ff2f4abe6077f80979
RoqiNazif <150114429+RoqiNazif@users.noreply.github.com>
2023-12-08 16:35:27 +0700

    roqinazif_2155201028

---
+++ tugas 7.php
@@ -0,0 +1,62 @@
+<?php
+class Mahasiswa {
+  // Properties
+  private $Nama;
+  private $NIM;
+  private $Mata_Kuliah;
+  private $Dosen_Killer;
+  private $Cita_cita;
+  private $Hobi;
+
+  // Methods
+  function __construct($Nama, $NIM, $Mata_Kuliah, $Dosen_Killer, $Cita_cita, $Hobi){
+  $this->Nama = $Nama;
+  $this->NIM = $NIM; 
+  $this->Mata_Kuliah = $Mata_Kuliah;
+  $this->Dosen_Killer = $Dosen_Killer;
+  $this->Cita_cita = $Cita_cita;
+  $this->Hobi = $Hobi;
+  }
+
+  function get_nama() {
+    return $this->Nama;
+  }
+  function get_nim() {
+    return $this->NIM;
+  }
+  function get_matkul() {
+    return $this->Mata_Kuliah;
+  }
+  function get_dokil() {
+    return $this->Dosen_Killer;
+  }
+  function get_cita() {
+    return $this->Cita_cita;
+  }
+  function get_hobi() {
+    return $this->Hobi;
+  }
+}
+
+// Universitas is inherited from Mahasiswa
+class Universitas extends Mahasiswa {
+  public function salam() {
+    echo "selamat datang dikampus kami. "; 
+  }
+}
+
+$selvi = new Universitas('Roqi Nazif', '2155201028','program berorientasi objek','bapak arif mudi priyatno','istri sunwoo','travelling'  );
+echo $roqi->salam();
+echo "<br>";
+echo $roqi->get_nama();
+echo "<br>";
+echo $roqi->get_nim();
+echo "<br>";
+echo $roqi->get_matkul();
+echo "<br>";
+echo $roqi->get_dokil();
+echo "<br>";
+echo $roqi->get_cita();
+echo "<br>";
+echo $roqi->get_hobi();
+?>
